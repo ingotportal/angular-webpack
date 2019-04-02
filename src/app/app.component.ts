584d12873ebffdfe65b6d61ad3577bdf07735d0a
mgill2190 <martin.gill@warrant-group.com>
2019-04-02 17:21:24 +0100

    added a button to change the text and a grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,39 @@
-import {Component} from "@angular/core";
+import {Component, ViewEncapsulation} from "@angular/core";
+import { RowClassArgs } from "@progress/kendo-angular-grid";
 
 @Component({
     selector: "hellow-app",
+    encapsulation: ViewEncapsulation.None,
+    styles: [`
+       .k-grid tr.even { background-color: #f45c42; }
+       .k-grid tr.odd { background-color: #41f4df; }
+   `],
     template: require("./app.component.html"),
 })
 export class HelloComponent {
+    public gridData: any[] = products;
     message = "Howdy Partner";
+
+    public rowCallback(context: RowClassArgs) {
+        const isEven = context.index % 2 == 0;
+        return {
+            even: isEven,
+            odd: !isEven,
+        };
+    }
+    onButtonClick() {
+        this.message = "YOU CLICKED THE BUTTON!!!";
+    }
 }
+const products = [{
+    ProductID: 1,
+    ProductName: "Chai",
+    UnitPrice: 18.0000,
+    Discontinued: true,
+}, {
+    ProductID: 2,
+    ProductName: "Chang",
+    UnitPrice: 19.0000,
+    Discontinued: false,
+},
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
+import {GridModule} from "@progress/kendo-angular-grid";
 import {HelloComponent} from "./app.component";
 
 @NgModule({
-    imports:      [BrowserModule, FormsModule], // import Angular's BrowserModule
+    imports:      [BrowserModule, FormsModule, GridModule], // import Angular's BrowserModule
     bootstrap:    [HelloComponent],  // indicate the bootstrap component
     declarations: [HelloComponent], // register our component with the module
 })
